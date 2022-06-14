b8e3a1b13ef46a8122d4f2964af7961b7e4042d3
Didizy <am_nikishin@mail.ru>
2022-06-14 12:00:25 +1000

    New map

--- vkr-ui/src/Components/Map.jsx
+++ vkr-ui/src/Components/Map.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import { Paper } from "@mui/material";
 import { Button } from "@mui/material";
 import { Container } from "@mui/system";
 import { NavLink } from "react-router-dom";
@@ -7,11 +8,16 @@ import { NavLink } from "react-router-dom";
 function Map() {
   return (
     <div>
+      <Container
+        maxWidth
+        component={Paper}
+        sx={{ p: 0.2, textAlign: "center", zIndex: 5, mb: 2 }}
+      >
+        <h2>Карта помещений</h2>{" "}
+      </Container>
       <Container maxWidth>
-        <Container>
-          <img src="imgs/map_d.jpg" alt="" class="map_img" />
-        </Container>
-        <Container>
+        <img src="imgs/D9.jpg" alt="" class="map_img" />
+        <Container sx={{ textAlign: "right" }}>
           <NavLink to="/" style={{ textDecoration: "none" }}>
             <Button>Назад</Button>
           </NavLink>
