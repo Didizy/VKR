a2f1a1c968a7536aeb026b6b5a8741ebc4cc9043
Didizy <am_nikishin@mail.ru>
2022-06-17 15:52:49 +1000

    Auto stash before merge of "main" and "origin/main"

--- API/cv_module.py
+++ API/cv_module.py
@@ -6,49 +6,49 @@ import cv2 as cv
 path = 'Areper'
 orb = cv.ORB_create(nfeatures=1500)
 images = []
-classNames=[]
-myList= os.listdir(path)
+classNames = []
+myList = os.listdir(path)
 
 for cl in myList:
-    imgCur=cv.imread(f'{path}/{cl}', 0)
+    imgCur = cv.imread(f'{path}/{cl}', 0)
     images.append(imgCur)
     classNames.append(os.path.splitext(cl)[0])
 
+
 def findDescriptor(images):
-    descList=[]
+    descList = []
     for image in images:
-        kp, des= orb.detectAndCompute(image, None)
+        kp, des = orb.detectAndCompute(image, None)
         descList.append(des)
     return descList
 
+
 def findID(img, desList):
-    kp2, des2=orb.detectAndCompute(img, None)
+    kp2, des2 = orb.detectAndCompute(img, None)
     bf = cv.BFMatcher()
-    matchList=[]
-    finalValue=-1
+    matchList = []
+    finalValue = -1
     try:
         for des1 in desList:
             matches = bf.knnMatch(des1, des2, k=2)
-            good=[]
+            good = []
             for m, n in matches:
-                if m.distance < 0.85*n.distance:
+                if m.distance < 0.85 * n.distance:
                     good.append([m])
             matchList.append(len(good))
     except:
         pass
-    if len(matchList)!=0:
+    if len(matchList) != 0:
         if max(matchList) > 20:
-            finalValue=matchList.index(max(matchList))
+            finalValue = matchList.index(max(matchList))
     return finalValue
 
+
 desList = findDescriptor(images)
-img_path='Allpic'
-myPic= os.listdir(img_path)
+img_path = 'Allpic'
+myPic = os.listdir(img_path)
 for pic in myPic:
     img = cv.imread(f'{img_path}/{pic}', 0)
-    id=findID(img, desList)
-    if id!=-1:
+    id = findID(img, desList)
+    if id != -1:
         print(pic, classNames[id])
-
-
-
