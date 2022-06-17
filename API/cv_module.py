feffd2770460bd0aab4d917a19220af233e3f7cb
JumpyDeldor <46585271+JumpyDeldor@users.noreply.github.com>
2022-06-17 16:48:11 +1000

    Update cv_module.py

--- API/cv_module.py
+++ API/cv_module.py
@@ -1,54 +1,55 @@
 import os
-
-import numpy as np
 import cv2 as cv
 
-path = 'Areper'
-orb = cv.ORB_create(nfeatures=1500)
-images = []
-classNames=[]
-myList= os.listdir(path)
 
-for cl in myList:
-    imgCur=cv.imread(f'{path}/{cl}', 0)
-    images.append(imgCur)
-    classNames.append(os.path.splitext(cl)[0])
-
-def findDescriptor(images):
-    descList=[]
+def findDescriptor(images, orb):
+    descList = []
     for image in images:
-        kp, des= orb.detectAndCompute(image, None)
+        kp, des = orb.detectAndCompute(image, None)
         descList.append(des)
     return descList
 
-def findID(img, desList):
-    kp2, des2=orb.detectAndCompute(img, None)
+
+def findID(img, desList, orb):
+    kp2, des2 = orb.detectAndCompute(img, None)
     bf = cv.BFMatcher()
-    matchList=[]
-    finalValue=-1
+    matchList = []
+    finalValue = -1
     try:
         for des1 in desList:
             matches = bf.knnMatch(des1, des2, k=2)
-            good=[]
+            good = []
             for m, n in matches:
-                if m.distance < 0.85*n.distance:
+                if m.distance < 0.85 * n.distance:
                     good.append([m])
             matchList.append(len(good))
     except:
         pass
-    if len(matchList)!=0:
+    if len(matchList) != 0:
         if max(matchList) > 20:
-            finalValue=matchList.index(max(matchList))
+            finalValue = matchList.index(max(matchList))
     return finalValue
 
-desList = findDescriptor(images)
-img_path='Allpic'
-myPic= os.listdir(img_path)
-for pic in myPic:
-    img = cv.imread(f'{img_path}/{pic}', 0)
-    id=findID(img, desList)
-    if id!=-1:
-        print(pic, classNames[id])
 
+class Classifier:
 
+    @staticmethod
+    def checkImage(image):
+        orb = cv.ORB_create(nfeatures=1500)
+        path = 'media/images/markers'
+        images = []
+        classNames = []
+        myList = os.listdir(path)
 
+        for cl in myList:
+            imgCur = cv.imread(f'{path}/{cl}', 0)
+            images.append(imgCur)
+            classNames.append(os.path.splitext(cl)[0])
+
+        desList = findDescriptor(images, orb)
+
+        id = findID(image, desList, orb)
+        if id != -1:
+            return classNames[id]
+        else:
+            return False
