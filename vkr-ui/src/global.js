09aac99287d7a78deeb9915c15b864b16f429d19
Didizy <am_nikishin@mail.ru>
2022-08-22 13:50:56 +1000

    update settings

--- vkr-ui/src/global.js
+++ vkr-ui/src/global.js
@@ -1,4 +1,4 @@
-global.local_path = "http://51.250.27.125/:8000";
+global.local_path = "http://51.250.27.125:8000";
 // global.local_path = "https://didizy.pythonanywhere.com";
 
 global.img_path = "http://51.250.27.125/:8000/media/";
